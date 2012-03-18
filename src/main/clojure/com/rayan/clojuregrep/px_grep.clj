2113f1554183181002cabdeeebd28521cc023ac0
unknown <nishant@.(none)>
2012-03-18 22:46:37 +0530

    Enhancement : nthreads is configurable Changing parameter to file instead of file-name for grep-files method.

--- src/main/clojure/com/rayan/clojuregrep/px_grep.clj
+++ src/main/clojure/com/rayan/clojuregrep/px_grep.clj
@@ -4,13 +4,14 @@
                                File InputStreamReader BufferedReader RandomAccessFile BufferedInputStream))
   (:use [clojure.java.io :only [reader writer]])
   (:gen-class true))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; PX Grep is a tool that will grep for an expression in a log directory. It parallelizes the grep across files,
 ; making it faster than normal grep
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn reader-gz [file-name skip-bytes]
+(defn reader-gz [file skip-bytes]
   (let [reader (-> (doto (java.util.zip.GZIPInputStream.
-                           (FileInputStream. file-name))
+                           (FileInputStream. file))
                      (.skip skip-bytes))
     (BufferedInputStream. 131072)
     (InputStreamReader. "US-ASCII")
@@ -31,15 +32,17 @@
     (with-open [r (reader-gz file 0)]
       (let [lines (filter (partial matches-line expr) (line-seq r))]
         (doseq [line lines]
-          (log (str line "\n")))))))
+          (log (str file ":" line "\n")))))))
 
-(defn grep-directory [directory expression]
-  (let [files-seq (tree-seq #(.isDirectory %) #(.listFiles %) (File. directory))
-        files-chunks (partition-all (long (/ (count files-seq) 10)) files-seq)
+(defn grep-directory [directory expression nthreads]
+  (let [files-seq (filter #(not (.isDirectory %)) (tree-seq #(.isDirectory %) #(.listFiles %) (File. directory)))
+        files-chunks (partition-all (long (/ (count files-seq) nthreads)) files-seq)
         grep-funcs (for [files-chunk files-chunks] #(grep-files files-chunk expression))]
-    (doall (apply pmap grep-funcs))))
+    (println files-seq)
+    (doall (apply pcalls grep-funcs))))
 
-(grep-files ["sample_file.txt.gz"] "Clojure")
+;(grep-files ["sample_file.txt.gz"] "Clojure")
+(grep-directory "G:/grep_dir" "social" 2)
 (.flush *out*)
 (shutdown-agents)
 
