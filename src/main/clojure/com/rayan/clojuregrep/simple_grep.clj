cd8c0519b22e6e178179b3c30b806947d0474da2
unknown <nishant@.(none)>
2012-03-18 09:52:01 +0530

    Fixed the write function to including \n after each line. Now the improve grep is actually slower. #fail Need to re-implement using RandomAccess seek.

--- src/main/clojure/com/rayan/clojuregrep/simple_grep.clj
+++ src/main/clojure/com/rayan/clojuregrep/simple_grep.clj
@@ -12,32 +12,35 @@
     (grep-lines lines expression)))
 
 (defn chunk-lines [lines n]
-  (loop [rem-lines lines chunked-lines []]
-    (if (<= (count rem-lines) n)
-      (conj chunked-lines rem-lines)
-      (let [first-chunk (take n rem-lines)]
-        (recur (drop n rem-lines) (conj chunked-lines first-chunk))))))
+  (lazy-seq
+    (loop [rem-lines lines chunked-lines []]
+      (if (<= (count rem-lines) n)
+        (conj chunked-lines rem-lines)
+        (let [first-chunk (take n rem-lines)]
+          (recur (drop n rem-lines) (conj chunked-lines first-chunk)))))))
 
 (defn grep-improved [file-name expression]
   "Improved grep"
-  (let [lines (line-seq (reader file-name))
-        chunked-lines (chunk-lines lines 1000)
-        grep-funs (for [chunk chunked-lines] #(grep-lines chunk expression))]
-    (apply pcalls grep-funs)))
+  (lazy-seq
+    (let [lines (line-seq (reader file-name))
+          chunked-lines (chunk-lines lines 1000)
+          grep-funs (for [chunk chunked-lines] #(grep-lines chunk expression))]
+      (apply pcalls grep-funs))))
 
 (defn write-file [file-name]
   (with-open [w (writer file-name)]
-    (doseq [line (interleave (repeat 100000 "grep this line") (repeat 1000000 "don't look at this line"))]
-      (.write w line))))
+    (doseq [line (interleave (repeat 1000000 "grep this line") (repeat 100000 "don't look at this line"))]
+      (.write w line)
+      (.write w "\n"))))
 (defn measure-time [func]
   (let [start (System/currentTimeMillis)]
     (func)
     (- (System/currentTimeMillis) start)))
 ;(write-file "something.txt")
-(println (for [i (range 1)] (measure-time #(quick-and-dirty-grep2 "something.txt" "grep"))))
-(println (count (quick-and-dirty-grep2 "something.txt" "grep")))
-(println (= (quick-and-dirty-grep2 "something.txt" "grep") (grep-improved "something.txt" "grep")))
-(println (count (grep-improved "something.txt" "grep")))
-(println (for [i (range 1)] (measure-time #(grep-improved "something.txt" "grep"))))
+(println (for [i (range 10)] (measure-time #(doall (quick-and-dirty-grep2 "something.txt" "grep")))))
+;(println (count (quick-and-dirty-grep2 "something.txt" "grep")))
+;(println (= (quick-and-dirty-grep2 "something.txt" "grep") (grep-improved "something.txt" "grep")))
+;(println (count (grep-improved "something.txt" "grep")))
+(println (for [i (range 10)] (measure-time #(doall (grep-improved "something.txt" "grep")))))
 (shutdown-agents)
 ;(println s)
